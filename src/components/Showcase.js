758111929d4a33b1043a9b0c237fc986477d3a92
Thomas Sankara <thomassankara74@gmail.com>
2021-07-09 16:20:05 +0300

    add framer motion animation

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,17 +1,28 @@
 import hero from "../images/hero.svg"
+import { motion } from "framer-motion"
 
 const Showcase = () => {
   return (
     <>
       <section className="showcase">
         <div className="overlay">
-          <article className="text">
+          <motion.article
+            className="text"
+            initial={{ x: "-100vw" }}
+            animate={{ x: 0 }}
+            transition={{ delay: 1, duration: 0.5 }}
+          >
             <h3>This is Downing</h3>
             <h1>The Minimalist CSS Framework</h1>
-          </article>
-          <article className="image">
+          </motion.article>
+          <motion.article
+            className="image"
+            initial={{ x: "-100vw" }}
+            animate={{ x: 0 }}
+            transition={{ delay: 0.2, duration: 1 }}
+          >
             <img src={hero} alt="html and css" />
-          </article>
+          </motion.article>
         </div>
       </section>
     </>
